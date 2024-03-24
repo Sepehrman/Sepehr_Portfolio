eeb819dd7e2c33cbd6e44c35acf8df791ffb165e
Sepehrman <sepehrman@icloud.com>
2024-03-24 11:17:06 -0700

    added description

--- src/components/ProjectGrid.vue
+++ src/components/ProjectGrid.vue
@@ -163,7 +163,7 @@ import { RouterLink, RouterView } from 'vue-router'
           </div>
           <div class="card-overlay">
             <div class="card-details">
-              <p>A web application that combats phishing cybercrime by demonstrating real-life examples and allowing users to participate in realistic scenarios.</p>
+              <p>A First Person Shooter game</p>
             </div>
           </div>
         </router-link>
@@ -172,49 +172,14 @@ import { RouterLink, RouterView } from 'vue-router'
       <div class="card-item">
         <router-link to='/TrashQueens'>
           <div class="card-title">
-            <h3>Trash Queens</h3>
+            <h3>Gouken</h3>
           </div>
           <div class="image-container">
             <img src="../assets/homepage-tq.jpg" alt="" />
           </div>
           <div class="card-overlay">
             <div class="card-details">
-              <p>An educational game promoting proper waste management practices by teaching players how to recycle effectively.
-                .</p>
-            </div>
-          </div>
-        </router-link>
-      </div>
-
-      <div class="card-item">
-        <router-link to='/CodeSnippet'>
-          <div class="card-title">
-            <h3>Code Snippet</h3>
-          </div>
-          <div class="image-container">
-            <img src="../assets/homepage-codeSnippet.jpg" alt="" />
-
-          </div>
-          <div class="card-overlay">
-            <div class="card-details">
-              <p>A CRUD React application designed for developers to share their code snippets with others and interact with other developers' code. </p>
-            </div>
-          </div>
-        </router-link>
-      </div>
-
-      <div class="card-item">
-        <router-link to='/Cibo'>
-          <div class="card-title">
-            <h3>My Cibo</h3>
-          </div>
-          <div class="image-container">
-            <img src="../assets/homepage-cibo.jpg" alt="" />
-          </div>
-          <div class="card-overlay">
-            <div class="card-details">
-              <p>A food management app that not only tracks expiration dates but also suggests recipes based on available ingredients and more.
-              </p>
+              <p>A 2D fighting game, built for iOS platforms using Swift</p>
             </div>
           </div>
         </router-link>
@@ -222,9 +187,6 @@ import { RouterLink, RouterView } from 'vue-router'
 
     </div>
   </div>
-
-
-
 
 
 </template>
